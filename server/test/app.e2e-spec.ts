84a8f96923b86aa5b44af43c42ad923b6abec11c
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2020-02-10 00:33:30 +0300

    Add database connection test env.

--- server/test/app.e2e-spec.ts
+++ server/test/app.e2e-spec.ts
@@ -2,13 +2,33 @@ import * as request from 'supertest';
 import { Test } from '@nestjs/testing';
 import { AppModule } from './../src/app.module';
 import { INestApplication } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from 'src/config/config.module';
+import { ConfigService } from 'src/config/config.service';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
   beforeAll(async () => {
     const moduleFixture = await Test.createTestingModule({
-      imports: [AppModule],
+      imports: [
+        TypeOrmModule.forRootAsync({
+          imports: [ConfigModule],
+          inject: [ConfigService],
+          useFactory: async (configService: ConfigService) => ({
+            type: configService.get('TYPEORM_CONNECTION') as any,
+            host: configService.get('TYPEORM_HOST'),
+            port: Number(configService.get('TYPEORM_PORT')),
+            username: configService.get('TYPEORM_USERNAME'),
+            password: configService.get('TYPEORM_PASSWORD'),
+            database: configService.get('TYPEORM_DATABASE'),
+            entities: ['dist/**/**.entity.js'],
+            synchronize: true,
+            dropSchema: true,
+          }),
+        }),
+        AppModule,
+      ],
     }).compile();
 
     app = moduleFixture.createNestApplication();
