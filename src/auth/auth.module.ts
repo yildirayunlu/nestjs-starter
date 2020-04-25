95e7b9d7731c3dcdab3c5725991588ae228c88ca
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2020-04-26 00:29:20 +0300

    Refactor config module. (#4)

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,14 +5,12 @@ import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { UserModule } from '../user/user.module';
-import { ConfigModule } from '../config/config.module';
 import { jwtConstants } from './constants';
 import { LocalStrategy } from './local.strategy';
 import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
-    ConfigModule,
     UserModule,
     PassportModule,
     JwtModule.register({
