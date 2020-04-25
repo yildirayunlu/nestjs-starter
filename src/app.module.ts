41e285bb42410fe0806d9c58fdf69e42fad9214c
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2020-04-26 00:42:12 +0300

    Path alias. (#3)
    * Add moduleNameMapper alias.
    
    * Refactor import paths.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
-import { AuthModule } from './auth/auth.module';
-import { PostModule } from './post/post.module';
-import { TypeOrmConfigService } from './factories/database.factory';
+import { AppController } from '@/app.controller';
+import { AppService } from '@/app.service';
+import { UserModule } from '@/user/user.module';
+import { AuthModule } from '@/auth/auth.module';
+import { PostModule } from '@/post/post.module';
+import { TypeOrmConfigService } from '@/factories/database.factory';
 
 @Module({
   imports: [
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,7 +4,7 @@ import { JwtModule } from '@nestjs/jwt';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { UserModule } from '../user/user.module';
+import { UserModule } from '@/user/user.module';
 import { jwtConstants } from './constants';
 import { LocalStrategy } from './local.strategy';
 import { JwtStrategy } from './jwt.strategy';
