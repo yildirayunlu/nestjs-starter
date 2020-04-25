9c032e117872e0aa2eda56006edcf3317d99f1ac
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2020-04-25 13:13:33 +0300

    Refactor config module.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,17 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { PostModule } from './post/post.module';
-import { ConfigModule } from './config/config.module';
 import { TypeOrmConfigService } from './factories/database.factory';
 
 @Module({
   imports: [
-    ConfigModule,
+    ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       useClass: TypeOrmConfigService,
--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,13 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { ConfigModule } from '../config/config.module';
 import { Post } from './post.entity';
 import { PostController } from './post.controller';
 import { PostService } from './post.service';
 
 @Module({
-  imports: [ConfigModule, TypeOrmModule.forFeature([Post])],
+  imports: [TypeOrmModule.forFeature([Post])],
   providers: [PostService],
   exports: [PostService],
   controllers: [PostController],
