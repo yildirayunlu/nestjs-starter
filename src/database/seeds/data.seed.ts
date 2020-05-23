b598d2d64b6647b8cc480115058ca3be4fb1b6ad
Yıldıray Ünlü <yildirayyunlu@gmail.com>
2020-05-23 21:57:59 +0300

    Updated seeder.

--- src/database/seeds/data.seed.ts
+++ src/database/seeds/data.seed.ts
@@ -4,12 +4,13 @@ import { User } from '@/user/entities/user.entity';
 import { Post } from '@/post/entities/post.entity';
 import { Comment } from '@/post/entities/comment.entity';
 
-export default class Posts implements Seeder {
+export default class Data implements Seeder {
   public async run(factory: Factory) {
     // #### Users ####
     await factory(User)().create({
       email: 'admin@mail.com',
       plainPassword: 'password',
+      roles: ['admin'],
       posts: [],
     });
 
@@ -17,19 +18,21 @@ export default class Posts implements Seeder {
     const user1 = await factory(User)().create({
       email: 'test1@mail.com',
       plainPassword: 'password',
-      // posts: await factory(Post)().createMany(10),
+      roles: ['user'],
     });
 
     // test user - 2
     const user2 = await factory(User)().create({
       email: 'test2@mail.com',
       plainPassword: 'password',
+      roles: ['user'],
     });
 
     // test user - 3
     const user3 = await factory(User)().create({
       email: 'test3@mail.com',
       plainPassword: 'password',
+      roles: ['user'],
     });
 
     // ### Posts ###
@@ -41,6 +44,33 @@ export default class Posts implements Seeder {
         }),
         await factory(Comment)().create({
           user: user3,
+        }),
+      ],
+    });
+
+    await factory(Post)().create({
+      user: user1,
+      comments: [
+        await factory(Comment)().create({
+          user: user2,
+        }),
+      ],
+    });
+
+    await factory(Post)().create({
+      user: user2,
+      comments: [
+        await factory(Comment)().create({
+          user: user1,
+        }),
+      ],
+    });
+
+    await factory(Post)().create({
+      user: user3,
+      comments: [
+        await factory(Comment)().create({
+          user: user2,
         }),
       ],
     });
